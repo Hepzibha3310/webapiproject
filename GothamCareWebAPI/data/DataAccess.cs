091a9de786c6cc3e925bbfd3ce8c007576529cb1
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-18 08:11:57 +0530

    Add files via upload

---
+++ GothamCareWebAPI/data/DataAccess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using charitableLibrary.Models;
+
+namespace charitableLibrary.Models
+{
+    public class DataAccess : IDataAcess
+    {
+        private readonly GothamCareContext  _context;
+        public DataAccess(GothamCareContext context)
+        {
+            _context = context;
+        }
+        public void AddOutletRecord(Outlet outlet)
+        {
+            _context.outlets.Add(outlet);
+            _context.SaveChanges();
+
+        }
+        public void UpdateOutletRecord(Outlet outlet)
+        {
+            _context.outlets.Update(outlet);
+            _context.SaveChanges();
+        }
+        
+
+        public void DeleteOutletRecord(string OutletID)
+        {
+            var entity = _context.outlets.FirstOrDefault (t => t.OutletID == OutletID);
+            _context.outlets.Remove(entity);
+            _context.SaveChanges();
+
+        }
+
+        
+
+        public Outlet GetOutletSingleRecord(string OutletID)
+        {
+            return _context.outlets.FirstOrDefault(t => t.OutletID == OutletID);
+        }
+
+
+        public List<Outlet> GetOutletRecord()
+        {
+            return _context.outlets.ToList();
+        }
+
+
+    }
+}
---
+++ GothamCareWebAPI/data/IDataAcess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace charitableLibrary.Models
+{
+    public interface IDataAcess
+    {
+        void AddOutletRecord(Outlet outlet);
+        void UpdateOutletRecord(Outlet outlet);
+        void DeleteOutletRecord(string OutletID);
+        Outlet GetOutletSingleRecord(string OutletID);
+        List<Outlet> GetOutletRecord();
+
+       
+
+    }
+}
+
