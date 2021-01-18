215f93317da8b48e7bc06aa5e4fb909ae92a6de3
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-18 08:14:37 +0530

    Add files via upload

---
+++ GothamCareWebAPI/Controllers/OutletsController.cs
@@ -0,0 +1,86 @@
+﻿using charitableLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCharityWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OutletsController : ControllerBase
+    {
+        private readonly IDataAcess _dataAcess;
+
+        public OutletsController(IDataAcess dataAcess)
+        {
+            _dataAcess = dataAcess;
+        }
+
+        [HttpGet]
+        public IEnumerable<Outlet> Get()
+        {
+            return _dataAcess.GetOutletRecord();
+        }
+
+        [HttpPost]
+        public IActionResult Create ([FromBody]Outlet outlet)
+        {
+          if (ModelState.IsValid)
+            {
+                Guid obj = Guid.NewGuid();
+                outlet.OutletID = obj.ToString();
+                _dataAcess.AddOutletRecord(outlet);
+                return Ok();
+
+            }
+            return BadRequest();
+        }
+        
+        [HttpGet ("{ID}")]
+        public Outlet Details(string OutletID)
+        {
+            return _dataAcess.GetOutletSingleRecord(OutletID);
+        }
+        
+        [HttpPut]
+        public IActionResult Edit([FromBody] Outlet outlet)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataAcess.UpdateOutletRecord(outlet);
+                return Ok();
+
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteConfirmed(string OutletID)
+        {
+            var data = _dataAcess.GetOutletSingleRecord(OutletID);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            _dataAcess.DeleteOutletRecord(OutletID);
+            return Ok();
+
+        }
+
+
+
+
+    }
+    
+
+             
+    
+
+
+
+}
+    
+
---
+++ GothamCareWebAPI/Controllers/VolunteersController.cs
@@ -0,0 +1,75 @@
+﻿using charitableLibrary.Models;
+using charitableLibrary.VolunteerData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCharityWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolunteersController : ControllerBase
+    {
+        private readonly IVolunteerDataAccess _volunteerdataAccess;
+
+        public VolunteersController(IVolunteerDataAccess volunteerdataAcess)
+        {
+            _volunteerdataAccess = volunteerdataAcess;
+        }
+
+        [HttpGet]
+        public IEnumerable<Volunteer> Get()
+        {
+            return _volunteerdataAccess.GetVolunteerRecord();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid obj = Guid.NewGuid();
+                volunteer.VolunteerID = obj.ToString();
+                _volunteerdataAccess.AddVolunteerRecord(volunteer);
+                return Ok();
+
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{ID}")]
+        public Volunteer Details(string volunteerID)
+        {
+            return _volunteerdataAccess.GetVolunteerSingleRecord(volunteerID);
+        }
+
+        [HttpPut]
+        public IActionResult Edit([FromBody] Volunteer volunteer)
+        {
+            if (ModelState.IsValid)
+            {
+                _volunteerdataAccess.UpdateVolunteerRecord(volunteer);
+                return Ok();
+
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteConfirmed(string VolunteerID)
+        {
+            var data = _volunteerdataAccess.GetVolunteerSingleRecord(VolunteerID);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            _volunteerdataAccess.DeleteVolunteerRecord(VolunteerID);
+            return Ok();
+
+        }
+
+    }
+}
