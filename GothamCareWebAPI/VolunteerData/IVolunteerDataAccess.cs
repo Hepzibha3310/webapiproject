221aa32e576eff683ae16320cfb1e49cd5037206
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-18 08:08:20 +0530

    Add files via upload

---
+++ GothamCareWebAPI/VolunteerData/IVolunteerDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using charitableLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace charitableLibrary.VolunteerData
+{
+    public interface IVolunteerDataAccess
+    {
+        void AddVolunteerRecord(Volunteer volunteer);
+        void UpdateVolunteerRecord(Volunteer outlet);
+        void DeleteVolunteerRecord(string VolunteerID);
+        Volunteer GetVolunteerSingleRecord(string VolunteerID);
+        List<Volunteer> GetVolunteerRecord();
+
+    }
+}
---
+++ GothamCareWebAPI/VolunteerData/VolunteerDataAccess.cs
@@ -0,0 +1,51 @@
+﻿using charitableLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace charitableLibrary.VolunteerData
+{
+    public class VolunteerDataAccess : IVolunteerDataAccess
+    {
+        private readonly GothamCareContext _context;
+        public VolunteerDataAccess(GothamCareContext context)
+        {
+            _context = context;
+        }
+        public void AddVolunteerRecord(Volunteer volunteer)
+        {
+            _context.volunteers.Add(volunteer);
+            _context.SaveChanges();
+
+        }
+        public void UpdateVolunteerRecord(Volunteer volunteer)
+        {
+            _context.volunteers.Update(volunteer);
+            _context.SaveChanges();
+        }
+
+
+        public void DeleteVolunteerRecord(string VolunteerID)
+        {
+            var entity = _context.volunteers.FirstOrDefault(t => t.VolunteerID == VolunteerID);
+            _context.volunteers.Remove(entity);
+            _context.SaveChanges();
+
+        }
+
+
+
+        public Volunteer GetVolunteerSingleRecord(string VolunteerID)
+        {
+            return _context.volunteers.FirstOrDefault(t => t.VolunteerID == VolunteerID);
+        }
+
+
+        public List<Volunteer>  GetVolunteerRecord()
+        {
+            return _context.volunteers.ToList();
+        }
+
+    }
+}
