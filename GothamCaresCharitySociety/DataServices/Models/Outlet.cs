e56343503bc91c9da7ee35c39b99b5108caa425c
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:17:45 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/DataServices/Models/Outlet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataServices.Models
+{
+    public class Outlet
+    {
+        [Key]
+        public int OutletID { get; set; }
+        [Required]
+        public string OutletName { get; set; }
+        [Required]
+        public string StreetName { get; set; }
+        [Required]
+        public string Landmark { get; set; }
+        [Required]
+        public int NumberOfAvailableFoodPacks { get; set; }
+        [Required]
+        public int NumberOfVolunteersRequired { get; set; }
+        [Required]
+        public string TypeOfFood { get; set; }
+        [Required]
+        public DateTime DateTime { get; set; }
+
+    }
+}
---
+++ GothamCaresCharitySociety/DataServices/Models/Volunteer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataServices.Models
+{
+    public class Volunteer
+    {
+        [Key]
+        public int VolunteerID { get; set; }
+        [Required]
+        public int OutletID { get; set; }
+        [Required]
+        public string VolunteerName { get; set; }
+        [Required]
+        public string VolunteerAddress { get; set; }
+        [Required]
+        public int VolunteerPhonenumber { get; set; }
+        [Required]
+        public string VolunteerEmail { get; set; }
+
+    }
+}
