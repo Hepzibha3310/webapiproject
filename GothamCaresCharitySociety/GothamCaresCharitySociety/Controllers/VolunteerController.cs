1f3e988f9320d31bba68c7faf730f74e17bf8e0d
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:09:08 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/GothamCaresCharitySociety/Controllers/VolunteerController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessServices.ServiceVolunteer;
+using DataServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCaresCharitableSociety.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolunteerController : ControllerBase
+    {
+        private IVolunteerData _volunteerData;
+        public VolunteerController(IVolunteerData volunteerData)
+        {
+            _volunteerData = volunteerData;
+        }
+
+        [HttpGet]
+        [Route("api/[Controller]")]
+        public IActionResult GetVolunteer()
+        {
+            return Ok(_volunteerData.GetVolunteer());
+        }
+        [HttpGet]
+        [Route("api/[Controller]/{id}")]
+        public IActionResult GetVolunteer(int VolunteerID)
+        {
+            var volunteer = _volunteerData.GetVolunteer(VolunteerID);
+            if (volunteer != null)
+            {
+                return Ok(volunteer);
+            }
+            return NotFound($"Outlet with id:{ VolunteerID } Was Not Found!");
+        }
+        [HttpPost]
+        [Route("api/[Controller]")]
+        public IActionResult AddVolunteer(Volunteer volunteer)
+        {
+            _volunteerData.AddVolunteer(volunteer);
+            return Ok(volunteer);
+        }
+        [HttpDelete]
+        [Route("api/[Controller]/{id}")]
+        public IActionResult DeleteVolunteer(int VolunteerID)
+        {
+            var volunteer = _volunteerData.GetVolunteer(VolunteerID);
+            if (volunteer != null)
+            {
+                _volunteerData.DeleteVolunteer(volunteer);
+                return Ok();
+            }
+            return NotFound(" Was Not Found!");
+        }
+    }
+}
