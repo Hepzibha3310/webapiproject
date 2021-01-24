5da3155b31372164f7933f4b487ac6dfcb79bc35
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:20:15 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/BudinessServices/ServiceAdmin/AdminData.cs
@@ -0,0 +1,24 @@
+﻿using DataServices;
+using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices.ServiceAdmin
+{
+    public class AdminData :IAdminData
+    {
+        private GothamCareContext _gothamCareContext;
+        public AdminData(GothamCareContext gothamCareContext)
+        {
+            _gothamCareContext = gothamCareContext;
+        }
+
+        public Admin login(string Email, string Password)
+        {
+            return _gothamCareContext.Admin.Find(Email); ;
+        }
+    }
+}
