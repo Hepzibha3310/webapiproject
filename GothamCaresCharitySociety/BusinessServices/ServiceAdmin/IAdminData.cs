cec6fa3c55db89e37b0db7e37a9c78641fb0d058
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:23:35 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/BusinessServices/ServiceAdmin/IAdminData.cs
@@ -0,0 +1,17 @@
+﻿using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessServices.ServiceAdmin
+{
+    public interface IAdminData
+    {
+        
+        Admin login(string Email, string Password);
+    }
+
+}
+
