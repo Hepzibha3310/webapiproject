b45e12d7c5d34720c12b47646f81190ecdb5037f
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:04:53 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/GothamCaresCharitySociety/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessServices.ServiceAdmin;
+using DataServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCaresCharitableSociety.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private IAdminData _adminData;
+        public AdminController(IAdminData adminData)
+        {
+            _adminData = adminData;
+        }
+
+        [HttpGet("Login/{Email}/{Password}")]
+        public IActionResult Login(string Email, string Password)
+        {
+            Admin admin = _adminData.login(Email, Password);
+            if (admin == null)
+            {
+                return NotFound("wrong");
+            }
+            else if (!(admin.Password).Equals(Password))
+            {
+                return NotFound("wrong");
+            }
+            else
+            {
+                return Ok("login success");
+            }
+        }
+    }
+}
---
+++ GothamCaresCharitySociety/GothamCaresCharitySociety/OutletController.cs
@@ -0,0 +1,59 @@
+﻿using BusinessServices.ServiceOutlet;
+using DataServices.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GothamCaresCharitableSociety.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OutletController : ControllerBase
+    {
+        private IOutletData _outletData;
+        public OutletController(IOutletData outletData)
+        {
+            _outletData = outletData;
+        }
+
+        [HttpGet]
+        [Route("api/[Controller]")]
+        public IActionResult GetOutlet()
+        {
+            return Ok(_outletData.GetOutlet());
+        }
+        [HttpGet]
+        [Route("api/[Controller]/{id}")]
+        public IActionResult GetOutlet(int OutletID)
+        {
+            var outlet = _outletData.GetOutlet(OutletID);
+            if (outlet != null)
+            {
+                return Ok(outlet);
+            }
+            return NotFound($"Outlet with id:{ OutletID } Was Not Found!");
+        }
+        [HttpPost]
+        [Route("api/[Controller]")]
+        public IActionResult AddOutlet(Outlet outlet)
+        {
+            _outletData.AddOutlet(outlet);
+            return Ok(outlet);
+        }
+        [HttpDelete]
+        [Route("api/[Controller]/{id}")]
+        public IActionResult DeleteOutlet(int OutletID)
+        {
+            var outlet = _outletData.GetOutlet(OutletID);
+            if (outlet != null)
+            {
+                _outletData.DeleteOutlet(outlet);
+                return Ok();
+            }
+            return NotFound(" Was Not Found!");
+        }
+    }
+}
