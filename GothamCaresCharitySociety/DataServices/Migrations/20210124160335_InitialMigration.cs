3513e2d895a92d19829c124b68b7130f5218ab0f
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:16:21 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/DataServices/Migrations/20210124160335_InitialMigration.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DataServices.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Outlet",
+                columns: table => new
+                {
+                    OutletID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OutletName = table.Column<string>(type: "text", nullable: false),
+                    StreetName = table.Column<string>(type: "text", nullable: false),
+                    Landmark = table.Column<string>(type: "text", nullable: false),
+                    NumberOfAvailableFoodPacks = table.Column<int>(type: "integer", nullable: false),
+                    NumberOfVolunteersRequired = table.Column<int>(type: "integer", nullable: false),
+                    TypeOfFood = table.Column<string>(type: "text", nullable: false),
+                    DateTime = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outlet", x => x.OutletID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Volunteer",
+                columns: table => new
+                {
+                    VolunteerID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OutletID = table.Column<int>(type: "integer", nullable: false),
+                    VolunteerName = table.Column<string>(type: "text", nullable: false),
+                    VolunteerAddress = table.Column<string>(type: "text", nullable: false),
+                    VolunteerPhonenumber = table.Column<int>(type: "integer", nullable: false),
+                    VolunteerEmail = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Volunteer", x => x.VolunteerID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "Outlet");
+
+            migrationBuilder.DropTable(
+                name: "Volunteer");
+        }
+    }
+}
