b09a3cc06ed8a9057e5494f89fd8583f57e96a5a
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:13:37 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/DataServices/GothamCareContext.cs
@@ -0,0 +1,31 @@
+﻿using DataServices.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataServices
+{
+    public class GothamCareContext:DbContext
+    {
+        public GothamCareContext(DbContextOptions<GothamCareContext> options) : base(options)
+        {
+
+        }
+
+        
+
+        public DbSet<Outlet> Outlet { get; set; }
+        public DbSet<Volunteer> Volunteer { get; set; }
+        public DbSet<Admin> Admin { get; set; }
+        
+
+        
+
+
+
+       
+        
+
+    }
+}
