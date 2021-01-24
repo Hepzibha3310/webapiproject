46ff5f09385050d53cf1614af23142c305552d56
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:26:24 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/BusinessServices/ServiceOutlet/IOutletData.cs
@@ -0,0 +1,17 @@
+﻿using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessServices.ServiceOutlet
+{
+   public interface IOutletData
+    {
+     
+        List<Outlet> GetOutlet();
+        Outlet GetOutlet(int OutletID);
+        Outlet AddOutlet(Outlet outlet);
+        void DeleteOutlet(Outlet outlet);
+    }
+}
+
---
+++ GothamCaresCharitySociety/BusinessServices/ServiceOutlet/OutletData.cs
@@ -0,0 +1,42 @@
+﻿using DataServices;
+using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessServices.ServiceOutlet
+{
+    public class OutletData :IOutletData
+    {
+        private GothamCareContext _gothamCareContext;
+        public OutletData(GothamCareContext gothamCareContext)
+        {
+            _gothamCareContext = gothamCareContext;
+        }
+
+        public Outlet AddOutlet(Outlet outlet)
+        {
+            _gothamCareContext.Outlet.Add(outlet);
+            _gothamCareContext.SaveChanges();
+            return outlet;
+        }
+
+        public void DeleteOutlet(Outlet outlet)
+        {
+            _gothamCareContext.Outlet.Remove(outlet);
+            _gothamCareContext.SaveChanges();
+        }
+
+        public Outlet GetOutlet(int OutletID)
+        {
+            var outlet = _gothamCareContext.Outlet.Find(OutletID);
+            return outlet;
+        }
+
+        public List<Outlet> GetOutlet()
+        {
+            return _gothamCareContext.Outlet.ToList();
+        }
+    }
+}
