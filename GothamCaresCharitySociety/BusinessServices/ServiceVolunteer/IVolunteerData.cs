65f197f62714338911fac8fb9a814a8a8eeb1878
Hepzibha3310 <76929242+Hepzibha3310@users.noreply.github.com>
2021-01-24 22:24:51 +0530

    Add files via upload

---
+++ GothamCaresCharitySociety/BusinessServices/ServiceVolunteer/IVolunteerData.cs
@@ -0,0 +1,15 @@
+﻿using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessServices.ServiceVolunteer
+{
+    public interface IVolunteerData
+    {
+        List<Volunteer> GetVolunteer();
+        Volunteer GetVolunteer(int VolunteerID);
+        Volunteer AddVolunteer(Volunteer volunteer);
+        void DeleteVolunteer(Volunteer volunteer);
+    }
+}
---
+++ GothamCaresCharitySociety/BusinessServices/ServiceVolunteer/VolunteerData.cs
@@ -0,0 +1,42 @@
+﻿using DataServices;
+using DataServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusinessServices.ServiceVolunteer
+{
+    public class VolunteerData :IVolunteerData
+    {
+        private GothamCareContext _gothamCareContext;
+        public VolunteerData(GothamCareContext gothamCareContext)
+        {
+            _gothamCareContext = gothamCareContext;
+        }
+        public Volunteer AddVolunteer(Volunteer volunteer)
+        {
+            _gothamCareContext.Volunteer.Add(volunteer);
+            _gothamCareContext.SaveChanges();
+            return volunteer;
+        }
+
+        public void DeleteVolunteer(Volunteer volunteer)
+        {
+            _gothamCareContext.Volunteer.Remove(volunteer);
+            _gothamCareContext.SaveChanges();
+        }
+
+
+
+        public Volunteer GetVolunteer(int VolunteerID)
+        {
+            var volunteer = _gothamCareContext.Volunteer.Find(VolunteerID);
+            return volunteer;
+        }
+        public List<Volunteer> GetVolunteer()
+        {
+            return _gothamCareContext.Volunteer.ToList();
+        }
+    }
+}
